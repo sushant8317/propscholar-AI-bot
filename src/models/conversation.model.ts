6b9fee88b79c4519d5cd451ef0db6f1c7664f02c
sushant8317 <s.saurav2006@gmail.com>
2025-11-17 16:22:01 +0530

    Update conversation.model.ts

--- src/models/conversation.model.ts
+++ src/models/conversation.model.ts
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 const ConversationSchema = new mongoose.Schema({
   userId: { type: String, required: true },
-  conversationId: { type: String, required: true },
-  userMessage: { type: String, required: true },
-  botResponse: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now },
-  confidence: { type: Number, default: 0 }
+  messages: { type: [String], default: [] },
 });
 
 export const Conversation = mongoose.model("Conversation", ConversationSchema);
